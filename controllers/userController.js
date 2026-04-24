9015112ac1eaf34b453bf680aff438390fc3cd20
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-24 21:48:24 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,9 +6,30 @@ exports.createtask = async (req, res) => {
     {
         return res.json({message:"Task is required"})
     }
-    const user = await Todo.create(req.body);
-    res.json(user);
+    const Task = await Todo.create(req.body);
+    res.json(Task);
   } catch (err) {
     res.json({ message: "Error creating task" });
+  }
+};
+
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Todo.find();
+    res.json(tasks);
+  } catch (err) {
+    res.json({ message: "Error fetching tasks" });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    const deleted = await Todo.findByIdAndDelete(req.params.id);
+    if (!deleted) {
+      return res.json({ message: "Task not found" });
+    }
+    res.json({ message: "Task deleted successfully" });
+  } catch (err) {
+    res.json({ message: "Error deleting task" });
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,7 @@ const express=require('express');
 const router=express.Router();
 
 router.post('/tasks',userController.createtask);
+router.get('/tasks', userController.getTasks);   
+router.delete('/tasks/:id', userController.deleteTask); 
 
-module.exports=router;
+module.exports = router;
