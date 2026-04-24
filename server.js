5b50d5b710fe7de5d7151e8d7e40839ef5def354
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-24 21:37:45 +0530

    updated

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express=require('express');
-const app=express();
 const mongoose=require('mongoose');
 const userRoutes=require('./routes/userRoutes');
+
+const app=express();
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/TodoApp')
