c8300972379c2d7a4cc28e29c8fe1f4bb726750a
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-25 21:04:06 +0530

    updated

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const userRoutes=require('./routes/userRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 
-const app=express();
+const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/TodoApp')
-.then(()=>{
-    console.log('connected to database');
-})
-.catch((err)=>{
-    console.error('Error connecting to database:', err);
+mongoose
+  .connect("mongodb://localhost:27017/TodoApp")
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to database:", err);
+  });
+
+app.use("/", userRoutes);
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
 });
-
-app.use("/",userRoutes);
-
-app.listen(3000,()=>{
-    console.log('server is running on port 3000');
-})
